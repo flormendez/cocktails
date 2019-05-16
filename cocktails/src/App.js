70204c1d4238782cd01a3aad7a1cb15780fa8c0d
salvador <juanss@lagash.com>
2019-05-16 16:19:42 -0300

    Get lists functions

--- cocktails/src/App.js
+++ cocktails/src/App.js
@@ -1,21 +1,25 @@
-<<<<<<< HEAD
-import React, { Component } from 'react';
-import './App.css';
-import moduleName from '../public/'
-=======
 import React, { Component } from "react";
-import Layout from './components/Layout/Layout'
-import NavBar from './components/NavBar/NavBar'
+import Layout from "./components/Layout/Layout";
+import NavBar from "./components/NavBar/NavBar";
 import "./App.css";
 
->>>>>>> 4952b23ae26c840bee4045e3a866d1299207766b
+import { Consumer, Provider } from "./Provider/Provider.js";
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-       <NavBar />
-       <Layout />
-      </div>
+      <Provider>
+        <div className="App">
+          <NavBar />
+          <Layout />
+          <Consumer>
+            {value => {
+              <p>{value.state}</p>
+            }}
+            
+          </Consumer>
+        </div>
+      </Provider>
     );
   }
 }
--- cocktails/src/Provider/Provider.js
+++ cocktails/src/Provider/Provider.js
@@ -1,16 +1,91 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
+const Context = React.createContext();
+const URL = "https://www.thecocktaildb.com/api/json/v1/1/";
 class Provider extends Component {
-    state = {
+  state = {
+    categories: [],
+    categoriesLoaded: false,
+    ingredients: [],
+    ingredientsLoaded: false,
+    drinks: [],
+    drinksLoaded: false,
+    selectedDrink: {}
+  };
 
-    }
+  getCategories = PATH => {
+    fetch(URL + PATH, {
+      method: "GET",
+      headers: new Headers({})
+    })
+      .then(res => res.json())
+      .then(result => {
+        this.setState({
+          categoriesLoaded: true,
+          categories: result.drinks.map(e => e.strCategory)
+        });
+      })
+      .catch(error => {
+        this.setState({
+          error
+        });
+      });
+  };
+
+  getCategories = PATH => {
+    fetch(URL + PATH, {
+      method: "GET",
+      headers: new Headers({})
+    })
+      .then(res => res.json())
+      .then(result => {
+        this.setState({
+          categoriesLoaded: true,
+          categories: result.drinks.map(e => e.strCategory)
+        });
+      })
+      .catch(error => {
+        this.setState({
+          error
+        });
+      });
+  };
+  getIngredients = PATH => {
+    fetch(URL + PATH, {
+      method: "GET",
+      headers: new Headers({})
+    })
+      .then(res => res.json())
+      .then(result => {
+        this.setState({
+          categoriesIngredients: true,
+          ingredients: result.drinks.map(e => e.strIngredient1)
+        });
+        console.log("antes")
+      })
+      .then(console.log("despues"))
+      .catch(error => {
+        this.setState({
+          error
+        });
+      });
+  };
+  componentDidMount() {
+    this.getCategories("list.php?c=list");
+    this.getIngredients("list.php?i=list");
+  }
   render() {
     return (
-      <div>
-        
-      </div>
-    )
+      <Context.Provider
+        value={{
+          state: this.state,
+          getCategories: this.getCategories
+        }}
+      >
+        {this.props.children}
+      </Context.Provider>
+    );
   }
 }
-
-export default Provider
+const Consumer = Context.Consumer;
+export { Consumer, Provider };
